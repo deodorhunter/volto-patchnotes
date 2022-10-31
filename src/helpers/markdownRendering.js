0d747f5ee1cfab8710e0ded096ccd69d901e8824
Martina Bustacchini <martina.bustacchini@redturtle.it>
2022-10-31 09:44:27 +0100

    wip

--- src/helpers/markdownRendering.js
+++ src/helpers/markdownRendering.js
@@ -1,7 +1,7 @@
 export const customRenderComponents = () => ({
   components: {
     h2: (props) => {
-      console.log(props);
+      console.log('porco', props);
       return (
         <h2 id={`header-version`} {...props}>
           {' '}
